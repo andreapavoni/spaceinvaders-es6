b117c4f83eb18f3703bdc9a58512cee18317dc94
Andrea Pavoni <andrea.pavoni@gmail.com>
2017-12-09 17:03:21 +0100

    add starfield

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function randomise() {
+  starfield.stop();
+  starfield.stars = Math.random()*1000 + 50;
+  starfield.minVelocity = Math.random()*30+5;
+  starfield.maxVelocity = Math.random()*50 + starfield.minVelocity;
+  starfield.start();
+}
+
+window.onload = function() {
+  var container = document.getElementById('container');
+  var starfield = new Starfield();
+  starfield.initialise(container);
+  starfield.start();
+}
